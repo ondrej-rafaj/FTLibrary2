7b8dbc6727e77162cc8bf171b83c1fb0ea1174c5
A.Papadakis <apapadakis@APapadakiss-Mac-mini.local>
2012-08-03 20:25:04 +0100

    Subscripting support for iOS < 6.0 now you can access and array like this (array[0]) and a dictionary like this (dictionary[@"key"])

---
+++ FTLibrary2/Sources/Categories/NSObject+PLGSubscripting.m
@@ -0,0 +1,13 @@
+//
+//  NSObject+PLGSubscripting.m
+//  FTLibrary2
+//
+//  Created by A.Papadakis on 03/08/2012.
+//
+//
+
+#import "NSObject+PLGSubscripting.h"
+
+@implementation NSObject (PLGSubscripting)
+
+@end
