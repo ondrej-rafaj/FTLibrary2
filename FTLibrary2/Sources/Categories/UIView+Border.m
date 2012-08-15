6eecbee479f0f275cf22988188be5d084868e675
A.Papadakis <apapadakis@APapadakiss-Mac-mini.local>
2012-08-15 10:50:50 +0100

    simplified the process of a adding borders, and made preparations for the next upgrade.

--- FTLibrary2/Sources/Categories/UIView+Border.m
+++ FTLibrary2/Sources/Categories/UIView+Border.m
@@ -9,13 +9,41 @@
 #import "UIView+Border.h"
 #import "UIView+Layout.h"
 #import <QuartzCore/QuartzCore.h>
+//#import <objc/runtime.h>
+static const NSString *shapeLayers = @"UIView+Border-ShapeLayer-Dictionary";
+@interface UIView (BorderPrivate)
 
+@end
 @implementation UIView (Border)
+#pragma mark - 
+#pragma mark Category Getters/Setters
+//- (void)setShapeLayers:(NSMutableDictionary *)shapeLayersDict
+//{
+//	objc_setAssociatedObject(self, &shapeLayers, shapeLayersDict
+//							 , OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+//}
+//- (NSMutableDictionary *)shapeLayers
+//{
+//	return objc_getAssociatedObject(self, &shapeLayers);
+//}
+//
+//- (void)startBorder
+//{
+//	[self setShapeLayers:[[NSMutableDictionary alloc]init]];
+//	
+//}
+
 - (void)setBorderWidth:(CGFloat)width andColor:(UIColor *)color andRadius:(CGFloat)rad ofType:(UIViewBorderType)type
 {
 #pragma mark TODO:Implement the rest
 	if (type == UIViewBorderTypeNone)
+	{
+		self.layer.borderColor = [UIColor clearColor].CGColor;
+		self.layer.borderWidth = 0.0f;
+		self.layer.cornerRadius = 0.0f;
 		return;
+	}
+		
 	else if (type == UIViewBorderTypeAll)
 	{
 		[self.layer setBorderColor:color.CGColor];
@@ -32,68 +60,40 @@
 		CGMutablePathRef path = CGPathCreateMutable();
 		CGMutablePathRef path2;
 		path2 = NULL;
-		if (type == UIViewBorderTypeLeft)//left
+		if (type >= UIViewBorderTypeLeft && type <= UIViewBorderTypeBottom)//Left Right Top Bottom
 		{
-			CGPathMoveToPoint(path, NULL, 0, 0);
-			CGPathAddLineToPoint(path, NULL, 0, self.height);
-		}
-		else if (type == UIViewBorderTypeRight)//right
-		{
-			CGPathMoveToPoint(path, NULL, self.width, 0);
-			CGPathAddLineToPoint(path, NULL, self.width, self.height);
-		}
-		else if (type == UIViewBorderTypeTop)//top
-		{
-			CGPathMoveToPoint(path, NULL, 0, 0);
-			CGPathAddLineToPoint(path, NULL, self.width, 0);
-		}
-		else if (type == UIViewBorderTypeBottom)//bottom
-		{
-			CGPathMoveToPoint(path, NULL, self.width, self.height);
-			CGPathAddLineToPoint(path, NULL, 0, self.height);
+			[self addBorderOfType:type toPath:path];
 		}
 		else if (type == UIViewBorderTypeTopLeft)//top - left
 		{
-			CGPathMoveToPoint(path, NULL, self.width, 0);
-			CGPathAddLineToPoint(path, NULL, 0, 0);
-			CGPathAddLineToPoint(path, NULL, 0, self.height);
+			[self addBorderOfType:UIViewBorderTypeTop toPath:path];
+			[self addBorderOfType:UIViewBorderTypeLeft toPath:path];
 		}
 		else if (type == UIViewBorderTypeTopRight)//top - right
 		{
-			CGPathMoveToPoint(path, NULL, 0, 0);
-			CGPathAddLineToPoint(path, NULL, self.width, 0);
-			CGPathAddLineToPoint(path, NULL, self.width, self.height);
+			[self addBorderOfType:UIViewBorderTypeTop toPath:path];
+			[self addBorderOfType:UIViewBorderTypeRight toPath:path];
 		}
 		else if (type == UIViewBorderTypeBottomLeft)// bottom-left
 		{
-			CGPathMoveToPoint(path, NULL, 0, 0);
-			CGPathAddLineToPoint(path, NULL, 0, self.height);
-			CGPathAddLineToPoint(path, NULL, self.width, self.height);
+			[self addBorderOfType:UIViewBorderTypeBottom toPath:path];
+			[self addBorderOfType:UIViewBorderTypeLeft toPath:path];
 		}
 		else if (type == UIViewBorderTypeBottomRight)
 		{
-			CGPathMoveToPoint(path, NULL, 0, self.height);
-			CGPathAddLineToPoint(path, NULL, self.width, self.height);
-			CGPathAddLineToPoint(path, NULL, self.width, 0);
+			[self addBorderOfType:UIViewBorderTypeBottom toPath:path];
+			[self addBorderOfType:UIViewBorderTypeRight toPath:path];
 		}
 		else if (type == UIViewBorderTypeTopBottom)
 		{
-			CGPathMoveToPoint(path, NULL, 0, 0);
-			CGPathAddLineToPoint(path, NULL, self.width, 0);
-			path2 = CGPathCreateMutable();
-			CGPathMoveToPoint(path2, NULL, self.width, self.height);
-			CGPathAddLineToPoint(path2, NULL, 0, self.height);
-			CGPathAddPath(path, NULL, path2);
+			[self addBorderOfType:UIViewBorderTypeTop toPath:path];
+			[self addBorderOfType:UIViewBorderTypeBottom toPath:path];
+
 		}
 		else if (type == UIViewBorderTypeLeftRight)
 		{
-			
-			CGPathMoveToPoint(path, NULL, 0, 0);
-			CGPathAddLineToPoint(path, NULL, 0, self.height);
-			path2 = CGPathCreateMutable();
-			CGPathMoveToPoint(path2, NULL, self.width, 0);
-			CGPathAddLineToPoint(path2, NULL, self.width, self.height);
-			CGPathAddPath(path, NULL, path2);
+			[self addBorderOfType:UIViewBorderTypeLeft toPath:path];
+			[self addBorderOfType:UIViewBorderTypeRight toPath:path];
 		}
 		
 		[_shapeLayer setPath:path];
@@ -114,4 +114,33 @@
 	}    
     //_shapeLayer is added as a sublayer of the view, the border is visible
 }
+
+- (void)addBorderOfType:(UIViewBorderType)type toPath:(CGMutablePathRef)path
+{
+	CGMutablePathRef path2 = CGPathCreateMutable();
+	if (type == UIViewBorderTypeLeft)//left
+	{
+		CGPathMoveToPoint(path2, NULL, 0, 0);
+		CGPathAddLineToPoint(path2, NULL, 0, self.height);
+	}
+	else if (type == UIViewBorderTypeRight)//right
+	{
+		CGPathMoveToPoint(path2, NULL, self.width, 0);
+		CGPathAddLineToPoint(path2, NULL, self.width, self.height);
+	}
+	else if (type == UIViewBorderTypeTop)//top
+	{
+		CGPathMoveToPoint(path2, NULL, 0, 0);
+		CGPathAddLineToPoint(path2, NULL, self.width, 0);
+	}
+	else if (type == UIViewBorderTypeBottom)//bottom
+	{
+		CGPathMoveToPoint(path2, NULL, self.width, self.height);
+		CGPathAddLineToPoint(path2, NULL, 0, self.height);
+	}
+	CGPathAddPath(path, NULL, path2);
+	CGPathRelease(path2);
+
+}
+
 @end
