6d571ac93ea20565d0652afb0f8d8015c0d4689c
Baldoph Pourprix <baldoph@fuerteint.com>
2013-02-19 11:42:14 +0000

    added NSString general category

---
+++ FTLibrary2/Sources/Categories/NSString+Tools.h
@@ -0,0 +1,15 @@
+//
+//  NSString_Tools.h
+//  FTLibrary2
+//
+//  Created by Baldoph Pourprix on 19/02/2013.
+//  Copyright (c) 2013 Fuerte International. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (Tools)
+
+-(NSString *)urlEncodeUsingEncoding:(NSStringEncoding)encoding;
+
+@end
---
+++ FTLibrary2/Sources/Categories/NSString+Tools.m
@@ -0,0 +1,21 @@
+//
+//  NSString_Tools.h
+//  FTLibrary2
+//
+//  Created by Baldoph Pourprix on 19/02/2013.
+//  Copyright (c) 2013 Fuerte International. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@implementation NSString (Tools)
+
+-(NSString *)urlEncodeUsingEncoding:(NSStringEncoding)encoding {
+	return (NSString *)CFBridgingRelease(CFURLCreateStringByAddingPercentEscapes(NULL,
+															   (CFStringRef)self,
+															   NULL,
+															   (CFStringRef)@"!*'\"();:@&=+$,/?%#[]% ",
+															   CFStringConvertNSStringEncodingToEncoding(encoding)));
+}
+
+@end
